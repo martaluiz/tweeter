32030537086a06a48289d7e201574992926d7712
Marta Luiz Avelino <martaluizavelino@gmail.com>
2020-02-26 23:44:25 +0000

    Use event.preventDefault() to prevent the default form submission behaviour, use .serialize() function and created a loadtweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,44 +6,20 @@
 
 // Fake data taken from initial-tweets.json
 
-$(document).ready(() => {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
-  const renderTweets = function (tweets) {
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
-    for (let tweet of tweets) {
+const renderTweets = function (tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  for (let tweet of tweets) {
+    $(document).ready(function () {
       $('.tweets-container').append(createTweetElement(tweet));
-    }
+    });
+
   }
-  const createTweetElement = function (tweet) {
-    let $tweet = `
+}
+const createTweetElement = function (tweet) {
+  let $tweet = `
 <article class="tweet">
         <header>
           <div>
@@ -70,9 +46,49 @@ $(document).ready(() => {
       </article>
 `
 
-    return $tweet;
-  }
+  return $tweet;
+}
 
-  renderTweets(data);
+//function to fetch tweets
+function loadTweets() {
+  $.ajax({
+    type: 'GET',
+    url: '/tweets',
+    success: function (tweet) {
+      renderTweets(tweet);
+    }
+  });
+}
+loadTweets();
 
+$(document).ready(function () {
+  $('#tweet-form').submit(function (event) {
+    // Stop the browser from submitting the form.
+    event.preventDefault();
+    // input validation, using notify.js for bad input
+    let length = $(this).find('#tweet-area').val().length;
+    if (length === 0) {
+      alert("OOps! Your tweet is empty! Must enter something. Get creative!");
+      return;
+    }
+    else if (length > 140) {
+      alert("OOps, your tweet is too long!");
+      return;
+    } else {
+
+      $.ajax({
+        type: 'POST',
+        url: '/tweets',
+        data: $(this).serialize(),
+        success: function () {
+          $('.tweets-container').empty();
+          $('textarea').val('');
+          $('.counter').html('140');
+          loadTweets();
+        }
+
+      });
+    }
+  });
 })
+renderTweets(data);
